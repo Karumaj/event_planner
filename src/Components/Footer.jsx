eb9537b665246f35df23df1585285dfd17538171
karumaj <karumaj@gmail.com>
2024-08-01 07:39:31 -0400

    Created Footer as a new componenet and added it

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const Footer = () => {
+        return (
+    <>
+    <footer>
+        <p>KJ's Event Planner &copy; 2024 </p>
+        <section>Bypass Business Park, Thika-Nrb Highway, POB 1234 - 00300, Juja Town, Kiambu, Kenya </section>
+        <section className='telephone'>Telephone: 0799000111</section>
+        <section className='email'>Email: kjems24@kj.net</section>
+           </footer>
+    </>
+)}
+
+export default Footer
